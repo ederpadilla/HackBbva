10cb26c0353ca3d1d6af932222aa62e65afefcab
edersinsinsin <eder.padilla97@gmail.com>
2016-10-30 08:15:03 -0600

    Modelos mejorados

--- app/src/main/java/jr/eder/developer/example/com/hackbbva/models/Cliente.java
+++ app/src/main/java/jr/eder/developer/example/com/hackbbva/models/Cliente.java
@@ -5,15 +5,61 @@ package jr.eder.developer.example.com.hackbbva.models;
  */
 
 public class Cliente {
+
     private int estado;
-    String id;
+    private String id;
+    private float enojo;
+    private float desprecio;
+    private float neutral;
+    private float felicidad;
 
     public Cliente(int estado, String id) {
         this.estado = estado;
         this.id = id;
     }
 
+    public Cliente(int estado, String id, float enojo, float desprecio, float neutral, float felicidad) {
+        this.estado = estado;
+        this.id = id;
+        this.enojo = enojo;
+        this.desprecio = desprecio;
+        this.neutral = neutral;
+        this.felicidad = felicidad;
+    }
+
     public Cliente() {
+    }
+
+    public float getEnojo() {
+        return enojo;
+    }
+
+    public void setEnojo(float enojo) {
+        this.enojo = enojo;
+    }
+
+    public float getDesprecio() {
+        return desprecio;
+    }
+
+    public void setDesprecio(float desprecio) {
+        this.desprecio = desprecio;
+    }
+
+    public float getNeutral() {
+        return neutral;
+    }
+
+    public void setNeutral(float neutral) {
+        this.neutral = neutral;
+    }
+
+    public float getFelicidad() {
+        return felicidad;
+    }
+
+    public void setFelicidad(float felicidad) {
+        this.felicidad = felicidad;
     }
 
     public Cliente(int estado) {
@@ -41,6 +87,10 @@ public class Cliente {
         return "Cliente{" +
                 "estado=" + estado +
                 ", id='" + id + '\'' +
+                ", enojo=" + enojo +
+                ", desprecio=" + desprecio +
+                ", neutral=" + neutral +
+                ", felicidad=" + felicidad +
                 '}';
     }
 }
--- app/src/main/java/jr/eder/developer/example/com/hackbbva/models/Sospechosos.java
+++ app/src/main/java/jr/eder/developer/example/com/hackbbva/models/Sospechosos.java
@@ -2,18 +2,68 @@ package jr.eder.developer.example.com.hackbbva.models;
 
 /**
  * Created by ederpadilla on 30/10/16.
+ *
+ models.add(new ArcProgressStackView.Model("Molestia", 70, Color.parseColor(bgColors[0]), mStartColors[3]));
+ models.add(new ArcProgressStackView.Model("Desprecio", 23, Color.parseColor(bgColors[1]), mStartColors[2]));
+ models.add(new ArcProgressStackView.Model("Disgusto", 34, Color.parseColor(bgColors[2]), mStartColors[1]));
+ models.add(new ArcProgressStackView.Model("Neutral", 23, Color.parseColor(bgColors[3]), mStartColors[0]));
  */
 
 public class Sospechosos {
     private int estado;
-    String id;
+    private String id;
+    private float enojo;
+    private float desprecio;
+    private float disgusto;
+    private float neutral;
 
     public Sospechosos(int estado, String id) {
         this.estado = estado;
         this.id = id;
     }
 
+    public Sospechosos(int estado, String id, float enojo, float desprecio, float disgusto, float neutral) {
+        this.estado = estado;
+        this.id = id;
+        this.enojo = enojo;
+        this.desprecio = desprecio;
+        this.disgusto = disgusto;
+        this.neutral = neutral;
+    }
+
     public Sospechosos() {
+    }
+
+    public float getEnojo() {
+        return enojo;
+    }
+
+    public void setEnojo(float enojo) {
+        this.enojo = enojo;
+    }
+
+    public float getDesprecio() {
+        return desprecio;
+    }
+
+    public void setDesprecio(float desprecio) {
+        this.desprecio = desprecio;
+    }
+
+    public float getDisgusto() {
+        return disgusto;
+    }
+
+    public void setDisgusto(float disgusto) {
+        this.disgusto = disgusto;
+    }
+
+    public float getNeutral() {
+        return neutral;
+    }
+
+    public void setNeutral(float neutral) {
+        this.neutral = neutral;
     }
 
     public Sospechosos(int estado) {
@@ -41,6 +91,10 @@ public class Sospechosos {
         return "Sospechosos{" +
                 "estado=" + estado +
                 ", id='" + id + '\'' +
+                ", enojo=" + enojo +
+                ", desprecio=" + desprecio +
+                ", disgusto=" + disgusto +
+                ", neutral=" + neutral +
                 '}';
     }
 }
